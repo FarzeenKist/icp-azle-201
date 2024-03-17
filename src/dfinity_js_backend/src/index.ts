0084eb1d8b39ed91ca03f4ce5e42945c88ecad12
FarzeenKist <farzeenkistnareddy@gmail.com>
2024-03-17 23:36:57 +0400

    Add Bank Canister

--- src/dfinity_js_backend/src/index.ts
+++ src/dfinity_js_backend/src/index.ts
@@ -1,191 +1,206 @@
-import { v4 as uuidv4 } from 'uuid';
-import { Server, StableBTreeMap, ic, Principal, serialize, Result } from 'azle';
-import express from 'express';
-import cors from 'cors';
-import { hexAddressFromPrincipal } from "azle/canisters/ledger";
+import {
+    query,
+    update,
+    text,
+    Record,
+    StableBTreeMap,
+    Variant,
+    Vec,
+    None,
+    Some,
+    Ok,
+    Err,
+    ic,
+    Principal,
+    Opt,
+    nat64,
+    Duration,
+    Result,
+    bool,
+    Canister,
+    init,
+  } from "azle";
+  import {
+    Ledger,
+    binaryAddressFromAddress,
+    binaryAddressFromPrincipal,
+    hexAddressFromPrincipal,
+  } from "azle/canisters/ledger";
+  //@ts-ignore
+  import { hashCode } from "hashcode";
+  import { v4 as uuidv4 } from "uuid";
 
-/**
- * This type represents a product that can be listed on a marketplace.
- * It contains basic properties that are needed to define a product.
- */
-class Product {
-    id: string;
-    title: string;
-    description: string;
-    location: string;
-    price: number;
-    seller: string;
-    attachmentURL: string;
-    soldAmount: number
-}
+const AccountType = Variant({
+    personal: text,
+    business: text,
+  });
+const TransactionType = Variant({
+    deposit: text,
+    withdrawal: text,
+    transfer: text
+  });
 
-class ProductPayload {
-    title: string;
-    description: string;
-    location: string;
-    price: number;
-    attachmentURL: string
-}
-
-enum OrderStatus {
-    PaymentPending,
-    Completed
-}
-
-class Order {
-    productId: string;
-    price: number;
-    status: string;
-    seller: string;
-}
-
-/**
- * `productsStorage` - it's a key-value data structure that is used to store products.
- * {@link StableBTreeMap} is a self-balancing tree that acts as durable data storage that keeps data across canister upgrades.
- * For the sake of this contract we've chosen {@link StableBTreeMap} as a storage for the next reasons:
- * - `insert`, `get` and `remove` operations have a constant time complexity - O(1)
- * - data stored in the map survives canister upgrades unlike using HashMap where data is stored in a heap and it's lost after the canister is upgraded
- * 
- * Breakdown of the `StableBTreeMap(string, Message)` datastructure:
- * - the key of the map is a `messageId`
- * - the value in this map is a message itself `Message` that is related to a given key (`messageId`)
- * 
- * Constructor values:
- * 1) 0 - memory id where to initialize a map.
- */
-const productsStorage = StableBTreeMap<string, Product>(0);
-const orders = StableBTreeMap<string, Order>(1);
-
-/* 
-    initialization of the ICRC Ledger canister. The principal text value is hardcoded because 
-    we set it in the `dfx.json`
-*/
-const ICRC_CANISTER_PRINCIPAL = "mxzaz-hqaaa-aaaar-qaada-cai";
-
-export default Server(() => {
-    const app = express();
-    // only for development purposes. For production-ready apps, one must configure CORS appropriately
-    app.use(cors());
-    app.use(express.json());
-
-    app.get("/products", (req, res) => {
-        res.json(productsStorage.values());
-    });
-
-    app.get("/orders", (req, res) => {
-        res.json(orders.values());
-    });
-
-    app.get("/products/:id", (req, res) => {
-        const productId = req.params.id;
-        const productOpt = productsStorage.get(productId);
-        if ("None" in productOpt) {
-            res.status(404).send(`the product with id=${productId} not found`);
-        } else {
-            res.json(productOpt.Some);
-        }
-    });
-
-    app.post("/products", (req, res) => {
-        const payload = req.body as ProductPayload;
-        const product = { id: uuidv4(), soldAmount: 0, seller: ic.caller().toText(), ...payload };
-        productsStorage.insert(product.id, product);
-        return res.json(product);
-    });
-
-    app.put("/products/:id", (req, res) => {
-        const productId = req.params.id;
-        const payload = req.body as ProductPayload;
-        const productOpt = productsStorage.get(productId);
-        if ("None" in productOpt) {
-            res.status(400).send(`couldn't update a product with id=${productId}. product not found`);
-        } else {
-            const product = productOpt.Some;
-            const updatedProduct = { ...product, ...payload, updatedAt: getCurrentDate() };
-            productsStorage.insert(product.id, updatedProduct);
-            res.json(updatedProduct);
-        }
-    });
-
-    app.delete("/products/:id", (req, res) => {
-        const productId = req.params.id;
-        const deletedProductOpt = productsStorage.remove(productId);
-        if ("None" in deletedProductOpt) {
-            res.status(400).send(`couldn't delete a product with id=${productId}. product not found`);
-        } else {
-            res.json(deletedProductOpt.Some);
-        }
-    });
-
-    /*
-        Before the order is received, the icrc2_approve is called - that's where we create
-        an allowance entry for the canister to make a transfer of an ICRC token on behalf of the sender to the seller of the product.
-        Here, we make an allowance transfer by calling icrc2_transfer_from.
-    */
-    app.post("/orders", async (req, res) => {
-        const productOpt = productsStorage.get(req.body.productId);
-        if ("None" in productOpt) {
-            res.send(`cannot create the order: product=${req.body.productId} not found`);
-        } else {
-            const product = productOpt.Some;
-            const allowanceResponse = await allowanceTransfer(product.seller, BigInt(product.price));
-            if (allowanceResponse.Err) {
-                res.send(allowanceResponse.Err);
-                return;
-            }
-            const order: Order = {
-                productId: product.id,
-                price: product.price,
-                status: OrderStatus[OrderStatus.Completed],
-                seller: product.seller
-            };
-            orders.insert(uuidv4(), order);
-            product.soldAmount += 1;
-            productsStorage.insert(product.id, product);
-            res.json(order);
-        }
-    });
-
-    /*
-        a helper function to get the address from the principal
-        the address is later used in the transfer method
-    */
-    app.get("/principal-to-address/:principalHex", (req, res) => {
-        const principal = Principal.fromText(req.params.principalHex);
-        res.json({ account: hexAddressFromPrincipal(principal, 0) });
-    });
-
-    return app.listen();
+const Transaction = Record({
+    id: text,
+    type: TransactionType,
+    amount: nat64,
+    spender: Opt(Principal),
+    from: Principal,
+    to: Opt(Principal)
 });
+const Account = Record({
+    id: text,
+    name: text,
+    balance: nat64,
+    owner: Principal,
+    accountType: AccountType,
+    spenders: Vec(Principal),
+    transactions: Vec(Transaction)
+});
+  
+const AccountPayload = Record({
+    name: text,
+    spenders: Opt(Vec(Principal)),
+    accountType: AccountType
+  });
+   
+  const Message = Variant({
+    NotFound: text,
+    NotOwner: text,
+    InvalidPayload: text,
+    PaymentFailed: text,
+    PaymentCompleted: text,
+  });
+  
+  const accountsStorage = StableBTreeMap(0, text, Account);
 
-function getCurrentDate() {
-    const timestamp = new Number(ic.time());
-    return new Date(timestamp.valueOf() / 1000_000);
-}
-
-async function allowanceTransfer(to: string, amount: bigint): Promise<Result<any, string>> {
-    try {
-        const response = await fetch(`icp://${ICRC_CANISTER_PRINCIPAL}/icrc2_transfer_from`, {
-            body: serialize({
-                candidPath: "/src/icrc1-ledger.did",
-                args: [{
-                    // for optional values use an empty array notation [] instead of None is they should remain empty
-                    spender_subaccount: [],
-                    created_at_time: [],
-                    memo: [],
-                    amount,
-                    fee: [],
-                    from: { owner: ic.caller(), subaccount: [] },
-                    to: { owner: Principal.fromText(to), subaccount: [] }
-                }]
-            })
-        });
-        return Result.Ok(response);
-    } catch (err) {
-        let errorMessage = "an error occurred on approval";
-        if (err instanceof Error) {
-            errorMessage = err.message;
-        }
-        return Result.Err(errorMessage);
+  let admin: typeof Principal;
+  
+  /* 
+      initialization of the Ledger canister. The principal text value is hardcoded because 
+      we set it in the `dfx.json`
+  */
+  const icpCanister = Ledger(Principal.fromText("ryjl3-tyaaa-aaaaa-aaaba-cai"));
+  
+  export default Canister({
+  initAdmin: init([], () => {
+    admin = ic.caller();
+  }),
+  getAccount: query([text], Result(Account, Message), (id) => {
+    const accountOpt = accountsStorage.get(id);
+    if ("None" in accountOpt) {
+      return Err({ NotFound: `Account with id=${id} not found` });
     }
-}
+    if (accountOpt.Some.owner.toString() !== ic.caller().toString()) {
+        return Err({NotOwner: `${ic.caller()} is not the owner of this account.`})
+    }
+    return Ok(accountOpt.Some);
+  }),
+    createAccount: update([AccountPayload], Result(Account, Message), (payload) => {
+        if (typeof payload !== "object" || Object.keys(payload).length === 0) {
+          return Err({ NotFound: "invalid payoad" });
+        }
+        const account = {
+          id: uuidv4(),
+          owner: ic.caller(),
+          transactions: [],
+          balance: 0,
+          ...payload,
+        };
+        accountsStorage.insert(account.id, account);
+        return Ok(account);
+      }),
+
+  });
+  
+  /*
+      a hash function that is used to generate correlation ids for orders.
+      also, we use that in the verifyPayment function where we check if the used has actually paid the order
+  */
+  function hash(input: any): nat64 {
+    return BigInt(Math.abs(hashCode().value(input)));
+  }
+  
+  // a workaround to make uuid package work with Azle
+  globalThis.crypto = {
+    // @ts-ignore
+    getRandomValues: () => {
+      let array = new Uint8Array(32);
+  
+      for (let i = 0; i < array.length; i++) {
+        array[i] = Math.floor(Math.random() * 256);
+      }
+  
+      return array;
+    },
+  };
+  
+  // to process refund of reservation fee to users.
+  async function makePayment(address: Principal, amount: nat64) {
+    const toAddress = hexAddressFromPrincipal(address, 0);
+    const transferFeeResponse = await ic.call(icpCanister.transfer_fee, {
+      args: [{}],
+    });
+    const transferResult = ic.call(icpCanister.transfer, {
+      args: [
+        {
+          memo: 0n,
+          amount: {
+            e8s: amount - transferFeeResponse.transfer_fee.e8s,
+          },
+          fee: {
+            e8s: transferFeeResponse.transfer_fee.e8s,
+          },
+          from_subaccount: None,
+          to: binaryAddressFromAddress(toAddress),
+          created_at_time: None,
+        },
+      ],
+    });
+    if ("Err" in transferResult) {
+      return Err({ PaymentFailed: `refund failed, err=${transferResult.Err}` });
+    }
+    return Ok({ PaymentCompleted: "refund completed" });
+  }
+  
+  function generateCorrelationId(productId: text): nat64 {
+    const correlationId = `${productId}_${ic.caller().toText()}_${ic.time()}`;
+    return hash(correlationId);
+  }
+  
+  /*
+      after the order is created, we give the `delay` amount of minutes to pay for the order.
+      if it's not paid during this timeframe, the order is automatically removed from the pending orders.
+  */
+  function discardByTimeout(memo: nat64, delay: Duration) {
+    ic.setTimer(delay, () => {
+      const order = pendingOrders.remove(memo);
+      console.log(`Order discarded ${order}`);
+    });
+  }
+  
+  async function verifyPaymentInternal(
+    sender: Principal,
+    amount: nat64,
+    block: nat64,
+    memo: nat64
+  ): Promise<bool> {
+    const blockData = await ic.call(icpCanister.query_blocks, {
+      args: [{ start: block, length: 1n }],
+    });
+    const tx = blockData.blocks.find((block) => {
+      if ("None" in block.transaction.operation) {
+        return false;
+      }
+      const operation = block.transaction.operation.Some;
+      const senderAddress = binaryAddressFromPrincipal(sender, 0);
+      const receiverAddress = binaryAddressFromPrincipal(ic.id(), 0);
+      return (
+        block.transaction.memo === memo &&
+        hash(senderAddress) === hash(operation.Transfer?.from) &&
+        hash(receiverAddress) === hash(operation.Transfer?.to) &&
+        amount === operation.Transfer?.amount.e8s
+      );
+    });
+    return tx ? true : false;
+  }
